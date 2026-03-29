b71cf4c28112a619edd0488d93cbf7fe98e22f74
saraelhabti12 <elhabti.sara@etu.uae.ac.ma>
2026-03-29 15:06:49 +0100

    fix before pull

--- exam/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Exam
-
-@admin.register(Exam)
-class ExamAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'student', 'date', 'score')
-    list_filter = ('subject', 'date')
-    search_fields = ('subject', 'student__first_name', 'student__last_name')
